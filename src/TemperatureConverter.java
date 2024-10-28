f146a784f525703bc5bca8e185018a045160537a
Gippp <aglifahalfb4@gmail.com>
2024-10-28 09:26:10 +0700

    commit1 branch1

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,37 +1,18 @@
 import java.util.Scanner;
-/**
- * Kelas TemperatureConverter menyediakan metode untuk mengonversi suhu dari Celcius
- * ke Fahrenheit, Kelvin, dan Reamur.
- *
- * <p>Pengguna dapat memasukkan suhu dalam Celcius dan memilih satu dari tiga opsi konversi
- * untuk melihat suhu dalam satuan yang diinginkan.</p>
- */
+
 
 class TemperatureConverter {
 
-    /** Konstanta opsi untuk Fahrenheit. */
+
     public static final int Fahrenheit_option = 1;
-    /** Konstanta opsi untuk Kelvin. */
     public static final int kelvin_option = 2;
-    /** Konstanta opsi untuk reamur. */
     public static final int reamur_option = 3;
 
-    /** Faktor konversi dari Celcius ke Fahrenheit. */
     private static final double Fahrenheit_Multiplier = 9.0 / 5.0;
-    /** Offset konversi dari Celcius ke Fahrenheit. */
     private static final double Fahrenheit_offset = 32;
-    /** Offset konversi dari Celcius ke Kelvin. */
     private static final double Kelvin_offset = 273.15;
-    /** Faktor konversi dari Celcius ke Reamur. */
     private static final double Reamur_Multiplier = 4.0 / 5.0;
 
-    /**
-     * Metode utama untuk menjalankan program.
-     * <p>Meminta input suhu dalam Celcius dan pilihan konversi dari pengguna,
-     * lalu memanggil metode konversi yang sesuai.</p>
-     *
-     * @param args argumen baris perintah (tidak digunakan)
-     */
 
 
     public static void main(String[] args) {
@@ -73,11 +54,6 @@ class TemperatureConverter {
         double reamur = celcius * Reamur_Multiplier;
         System.out.println("Suhu dalam Reamur: " + reamur);
     }
-    /**
-     * Mengonversi suhu dari Celcius ke Kelvin.
-     *
-     * @param celcius nilai suhu dalam Celcius yang akan dikonversi
-     */
 
     private static void ConvertToKelvin(double celcius) {
         double kelvin = celcius + Kelvin_offset;
